54b5ce478e3dae64849f7a17c6ef42a748329604
andreaspencer <andreaaddawn@gmail.com>
2022-01-09 12:30:27 -0800

    header added

---
+++ src/components/Header/index.js
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+import Nav from "../Nav";
+import About from "../About";
+import Contact from "../Contact";
+import Portfolio from '../Portfolio';
+import Resume from '../Resume';
+
+function Header() {
+    const [currentPage, handlePageChange] = useState("About");
+
+    const renderPage = () => {
+        switch (currentPage) {
+            case "About":
+                return <About />;
+            case "Portfolio":
+                return <Portfolio />;
+            case "Contact":
+                return <Contact />;
+            case "Resume":
+                return <Resume />
+            default:
+                return <About />;
+        }
+    };
+    return (
+        <div>
+            <nav className="navbar">
+                <div className="navbar-brand">
+                    <a className="navbar-item" href="https://github.com/andreaspencer/">
+                        <span className="">Andrea Spencer</span>
+                    </a>
+                </div>
+            </nav>
+            <Nav currentPage={currentPage} handlePageChange={handlePageChange} />
+            <main>
+                <div>{renderPage(currentPage)}</div>
+            </main>
+        </div>
+    );
+}
+
+export default Header;

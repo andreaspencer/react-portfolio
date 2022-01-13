c8030ff66c07afa75a126eb34cb565b96b0f53f4
andreaspencer <andreaaddawn@gmail.com>
2022-01-12 18:28:38 -0800

    directory chnage

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,9 @@
 import React, { useState } from "react";
-import Nav from "../Nav";
-import About from "../About";
-import Contact from "../Contact";
-import Portfolio from '../Portfolio';
-import Resume from '../Resume';
+import Nav from "./Nav";
+import About from "./About";
+import Contact from "./Contact";
+import Portfolio from './Portfolio';
+import Resume from './Resume';
 
 function Header() {
     const [currentPage, handlePageChange] = useState("About");
